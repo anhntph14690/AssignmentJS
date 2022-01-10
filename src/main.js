bdf4423965571a618c8ca13cdadbba8dbfc6710f
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-10 17:16:37 +0700

    index

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Navigo from "navigo";
+import AboutPage from "./pages/about";
 import HomePage from "./pages/home";
 
 const router = new Navigo("/", { linksSelector: "a" });
@@ -12,36 +13,10 @@ router.on({
         print(HomePage.render());
     },
     "/about": () => {
-        print("About Page");
+        print(AboutPage.render());
     },
     "/product": () => {
 
     },
 });
 router.resolve();
-
-// class KhuanBanh {
-//     constructor(luongDuong, luongBot) {
-//         this.luongDuong = luongDuong;
-//         this.luongBot = luongBot;
-//     }
-
-//     showInfo() {
-//         console.log(`
-//             Đường: ${this.luongDuong}
-//             Bột: ${this.luongBot}
-//         `);
-//     }
-// }
-
-// const banhDeo = new KhuanBanh(10, 20);
-// const banhNuong = new KhuanBanh(20, 30);
-
-// console.log(banhDeo.luongDuong);
-// banhDeo.showInfo();
-// banhNuong.showInfo();
-
-// function sum(a,b){
-// const numberA = a;
-// }
-// sum(3,4)
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,16 @@
+import Banner from "../components/banner";
+import NewsList from "../components/newsList";
+
 const HomePage = {
     render() {
-        return `
-            
+        return /* html */`
+            <div class="banner">
+                ${Banner.render()}
+            </div>
+            <div>
+                ${NewsList.render()}
+            </div>
         `;
-        
     },
 };
 export default HomePage;
