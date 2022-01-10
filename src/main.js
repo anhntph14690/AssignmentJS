8955c9242c56a5a1cf9f9d2c2c2167ed1c9d859a
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-10 12:44:56 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,44 @@
-import './style.css'
+import Navigo from "navigo";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const router = new Navigo("/");
+
+router.on({
+    "/": () => {
+        console.log("Home Page");
+    },
+    "/about": () => {
+        console.log("About Page");
+    },
+});
+router.resolve();
+
+// class KhuanBanh {
+//     constructor(luongDuong, luongBot) {
+//         this.luongDuong = luongDuong;
+//         this.luongBot = luongBot;
+//     }
+
+//     showInfo() {
+//         console.log(`
+//             Đường: ${this.luongDuong}
+//             Bột: ${this.luongBot}
+//         `);
+//     }
+// }
+
+// const banhDeo = new KhuanBanh(10, 20);
+// const banhNuong = new KhuanBanh(20, 30);
+
+// console.log(banhDeo.luongDuong);
+// banhDeo.showInfo();
+// banhNuong.showInfo();
+
+// function sum(a,b){
+// const numberA = a;
+// }
+// sum(3,4)
+
+// const person = {
+//     name: "Le Trong Dat",
+//     "/": function () { },
+// };
