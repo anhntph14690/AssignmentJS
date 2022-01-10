66919602dca6607226ce56bf87f7974c89ecb217
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-10 12:58:13 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Navigo from "navigo";
+import HomePage from "./pages/home";
 
-const router = new Navigo("/");
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("app").innerHTML = content;
+};
 
 router.on({
     "/": () => {
-        console.log("Home Page");
+        print(HomePage.render());
     },
     "/about": () => {
-        console.log("About Page");
+        print("About Page");
+    },
+    "/product": () => {
+
     },
 });
 router.resolve();
@@ -37,8 +45,3 @@ router.resolve();
 // const numberA = a;
 // }
 // sum(3,4)
-
-// const person = {
-//     name: "Le Trong Dat",
-//     "/": function () { },
-// };
