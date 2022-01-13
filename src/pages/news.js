ee474e7976a27bca3c2e61ac4b4858f269f13729
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-14 00:10:08 +0700

    Update

--- src/pages/news.js
+++ src/pages/news.js
@@ -40,14 +40,14 @@ const NewsPage = {
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="flex items-center">
                                 <div class="flex-shrink-0 h-10 w-10">
-                                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                                    <img class="h-10 w-10 rounded-full" src="${post.img}" alt="">
                                 </div>
                                 <div class="ml-4">
                                     <div class="text-sm font-medium text-gray-900">
-                                    Jane Cooper
+                                    ${post.title}
                                     </div>
                                     <div class="text-sm text-gray-500">
-                                    jane.cooper@example.com
+                                    anhntph14690@fpt.edu.vn
                                     </div>
                                 </div>
                                 </div>
@@ -68,8 +68,6 @@ const NewsPage = {
                                 <a href="/admin/news/${post.id}/edit" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                             </td>
                             </tr>
-
-                            <!-- More people... -->
                         </tbody>
                         </table>
                     </div>
