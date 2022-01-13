cc66ba340026b471a50580744d8490cbdb9ae7b6
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-14 00:22:26 +0700

    Update

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -18,9 +18,10 @@ const Edit = {
                 <div class="md:grid md:grid-cols-3 md:gap-6">
                     <div class="md:col-span-1">
                     <div class="px-4 sm:px-0">
-                        <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+                        <h3 class="text-lg font-medium leading-6 text-gray-900">${result.title}</h3>
                         <p class="mt-1 text-sm text-gray-600">
-                        Use a permanent address where you can receive mail.
+                        <img class="h-60 w-60 rounded-full" src="${result.img}" alt="">
+
                         </p>
                     </div>
                     </div>
