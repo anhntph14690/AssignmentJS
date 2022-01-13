5e6ee61f16a3da3486cce6df8b4a9fff1099cdc8
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-14 01:15:41 +0700

    Update

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,12 +2,13 @@ const AdminPage = {
     render() {
         return /*html*/`
             <h1 class="text-2xl my-10 font-bold">ADMIN</h1>
-            <div class="max-w-screen-md m-auto columns-2">
-                <a href="/admin/dashboard" class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
-                    <p class="ml-20 text-zinc-50 italic font-mono">Dashboard</p>
+            <div class="max-w-md m-auto ">
+
+                <a href="/admin/dashboard" class="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <p>Dashboard</p>
                 </a>
-                <a href="/admin/news" class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
-                    <p class="ml-28 text-zinc-50 italic font-mono">News</p>
+                <a href="/admin/news" class="mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <p>News</p>
                 </a>
             </div>
         `;
