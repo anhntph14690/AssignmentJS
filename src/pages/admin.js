c6b189f678783194a7dd2b6cab3a8892bcfba3e5
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-14 00:45:35 +0700

    Update

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -3,12 +3,12 @@ const AdminPage = {
         return /*html*/`
             <h1 class="text-2xl my-10 font-bold">ADMIN</h1>
             <div class="max-w-screen-md m-auto columns-2">
-            <div class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
-                <a href="/admin/dashboard" class="ml-20 text-zinc-50 italic font-mono">Dashboard</a>
-            </div>
-            <div class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
-                <a href="/admin/news" class="ml-28 text-zinc-50 italic font-mono">News</a>
-            </div>
+                <a href="/admin/dashboard" class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
+                    <p class="ml-20 text-zinc-50 italic font-mono">Dashboard</p>
+                </a>
+                <a href="/admin/news" class="bg-[#272f54] block py-3 px-5 text-4xl hover:bg-[#cb7802] border-2 border-black">
+                    <p class="ml-28 text-zinc-50 italic font-mono">News</p>
+                </a>
             </div>
         `;
     },
