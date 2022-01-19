40176f79b235da5e107e0e3376650fe930b3910d
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-19 23:37:14 +0700

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ const Footer = {
     render() {
         return `
         <footer class="bg-blue-800 text-center py-4 mt-4">
-            <p class="m-0 text-white">Copyright by Datlt</p>
+            <p class="m-0 text-white">Copyright by anhntph14690</p>
         </footer>
         `;
     },
