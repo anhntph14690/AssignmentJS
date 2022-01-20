bdfa8f52606802bd3e2fc9e60367bc63b65ec108
anhntph14690 <77096990+anhntph14690@users.noreply.github.com>
2022-01-20 22:22:56 +0700

    Update

--- src/components/newsList.js
+++ src/components/newsList.js
@@ -2,7 +2,7 @@
 
 const NewsList = {
     render() {
-        return fetch("https://61e7b3a8e32cd90017acbca5.mockapi.io/anhntph14690")
+        return fetch("https://5e79b4b817314d00161333da.mockapi.io/posts")
             .then((response) => response.json())
             .then((data) =>/*html*/`
                 <h2 class="font-semibold text-2xl uppercase text-blue-800 my-4">Tin tức học tập</h2>
@@ -10,9 +10,10 @@ const NewsList = {
                     ${data.map((post) => `
                         <div class="border p-4">
                             <a href="/news/${post.id}">
-                                <img src="${post.avatar}" alt="" />
+                                <img src="${post.img}" alt="" />
                             </a>
-                            <h3 class="my-3"><a  href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.name}</a></h3>           
+                            <h3 class="my-3"><a  href="/news/${post.id}" class="font-semibold text-lg text-orange-500">${post.title}</a></h3>           
+                            <p class="">${post.desc}</p>
                         </div>
                     `).join("")}
                 </div>
--- src/pages/detail.js
+++ src/pages/detail.js
@@ -2,11 +2,12 @@
 
 const DetailNewsPage = {
     render(id) {
-        return fetch(`https://61e7b3a8e32cd90017acbca5.mockapi.io/anhntph14690/${id}`)
+        return fetch(`https://5e79b4b817314d00161333da.mockapi.io/posts/${id}`)
             .then((response) => response.json())
             .then((data) => `
-                <h1>${data.name}</h1>
-                <img src="${data.avatar}" />
+                <h1>${data.title}</h1>
+                <img src="${data.img}" />
+                <p>${data.desc}</p>
             `);
 
         // const result = data.find((post) => post.id === id);
